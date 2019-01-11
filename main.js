783d5a6e93feb1f998333ebd8442ef514938224c
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2019-01-11 13:04:42 +0530

    Created Paddle class

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
+class Paddle {
+  constructor(height, width, left, bottom) {
+    this.height = height;
+    this.width = width;
+    this.left = left;
+    this.bottom = bottom;
+  }
+}
+
 const addPixelSufix = value => value + "px";
 
-const drawPaddle = function() {
-  let paddle = document.getElementById("paddle");
-  paddle.style.height = addPixelSufix(20);
-  paddle.style.width = addPixelSufix(200);
-  paddle.style.bottom = addPixelSufix(0);
-  paddle.style.left = addPixelSufix(380);
+const drawPaddle = function(paddle) {
+  let paddleDiv = document.getElementById("paddle");
+  paddleDiv.style.height = addPixelSufix(paddle.height);
+  paddleDiv.style.width = addPixelSufix(paddle.width);
+  paddleDiv.style.left = addPixelSufix(paddle.left);
+  paddleDiv.style.bottom = addPixelSufix(paddle.bottom);
 };
 
 const createPaddle = function() {
-  let paddle = document.createElement("div");
-  paddle.id = "paddle";
-  paddle.className = "paddle";
-  document.getElementById("screen").appendChild(paddle);
-  drawPaddle();
+  let paddleDiv = document.createElement("div");
+  paddleDiv.id = "paddle";
+  paddleDiv.className = "paddle";
+  document.getElementById("screen").appendChild(paddleDiv);
+  let paddle = new Paddle(20, 200, 380, 5);
+  drawPaddle(paddle);
 };
 
 const initialize = function() {
