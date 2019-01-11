40399dbbba2d14fc9c8aaf90e94d38d1dd29c5e0
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2019-01-11 14:19:03 +0530

    made the paddle move left or right

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const ARROW_LEFT = "ArrowLeft";
+const ARROW_RIGHT = "ArrowRight";
+
 class Paddle {
   constructor(height, width, left, bottom) {
     this.height = height;
@@ -5,12 +8,20 @@ class Paddle {
     this.left = left;
     this.bottom = bottom;
   }
+
+  moveLeft() {
+    this.left -= 5;
+  }
+
+  moveRight() {
+    this.left += 5;
+  }
 }
 
 const addPixelSufix = value => value + "px";
 
 const drawPaddle = function(paddle) {
-  let paddleDiv = document.getElementById("paddle");
+  let paddleDiv = document.getElementById("paddle_1");
   paddleDiv.style.height = addPixelSufix(paddle.height);
   paddleDiv.style.width = addPixelSufix(paddle.width);
   paddleDiv.style.left = addPixelSufix(paddle.left);
@@ -19,15 +30,34 @@ const drawPaddle = function(paddle) {
 
 const createPaddle = function() {
   let paddleDiv = document.createElement("div");
-  paddleDiv.id = "paddle";
+  paddleDiv.id = "paddle_1";
   paddleDiv.className = "paddle";
   document.getElementById("screen").appendChild(paddleDiv);
   let paddle = new Paddle(20, 200, 380, 5);
   drawPaddle(paddle);
+  return paddle;
+};
+
+const movePaddle = function(paddle) {
+  if (event.key == ARROW_LEFT) {
+    paddle.moveLeft();
+    drawPaddle(paddle);
+  }
+  if (event.key == ARROW_RIGHT) {
+    paddle.moveRight();
+    drawPaddle(paddle);
+  }
+};
+
+const addKeyListener = function(paddle) {
+  let screen = document.getElementById("screen");
+  screen.focus();
+  screen.onkeydown = movePaddle.bind(null, paddle);
 };
 
 const initialize = function() {
-  createPaddle();
+  let paddle = createPaddle();
+  addKeyListener(paddle);
 };
 
 window.onload = initialize;
