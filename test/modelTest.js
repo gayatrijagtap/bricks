e31c7579487a9deb2d31d624463a1b8858cdfabf
Gayatri Jagtap <44019996+gayatrijagtap@users.noreply.github.com>
2019-01-13 13:42:03 +0530

    Introduced src,test and style_sheet directories

--- test/modelTest.js
+++ test/modelTest.js
@@ -1,19 +1,19 @@
 describe("Paddle", function() {
   let paddle;
   beforeEach(function() {
-    paddle = new Paddle(10, 10, 10, 10);
+    paddle = new Paddle(10, 10, 10, 10, 10);
   });
   describe("moveLeft", function() {
     it("should reduce paddle left by 5 units", function() {
       paddle.moveLeft();
-      chai.assert.equal(5, paddle.left);
+      chai.assert.equal(0, paddle.left);
     });
   });
 
   describe("moveRight", function() {
     it("should increase paddle left by 5 units", function() {
       paddle.moveRight();
-      chai.assert.equal(15, paddle.left);
+      chai.assert.equal(20, paddle.left);
     });
   });
 });
